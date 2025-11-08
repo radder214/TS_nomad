a1fb249647852b9a32a713c11aa5b314ddd8eab6
radder214 <garden940316@gmail.com>
2025-11-08 15:29:52 +0900

    generic 2개인 경우 추가 & fewer parameter compatible

--- 8. Polymorphism.js
+++ 8. Polymorphism.js
@@ -13,3 +13,14 @@ const a = superPrint2([1, 2, 3]);
 const b = superPrint2([true, false, true]);
 const c = superPrint2(["a", "b", "c"]);
 const d = superPrint2([1, 2, true, false]);
+// "왜 파라미터가 (a) 밖에 없지? (a, b) 2개여야 하는거 아냐?" 라는 생각이 들것이다.
+// 그때는 typescript의 fewer parameter compatible을 검색해보자
+// 아래 공식 문서 사이트 접속 후 ctrl + f 로 fewer parameters 검색
+// https://www.typescriptlang.org/docs/handbook/2/functions.html#construct-signatures
+const genericTwoFn = (a) => a[0];
+// generic 대신 any를 써도 Error 자체는 발생하지 않는다.
+// 하지만 그러면 TS를 쓰는 의미가 없다.
+const a_genericTwo = genericTwoFn([1, 2, 3, 4], "x");
+const b_genericTwo = genericTwoFn([true, false, true], 1);
+const c_genericTwo = genericTwoFn(["a", "b", "c"], false);
+const d_genericTwo = genericTwoFn([1, 2, true, false, "hello"], []);
